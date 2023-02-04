7583258e3a2b0c397b8900c90a17bec98802bfd1
julschoen <julianschoen@gmx.de>
2023-02-04 12:21:46 +0100

    Update stats.py

--- stats.py
+++ stats.py
@@ -3,6 +3,39 @@ import torch
 from torchvision import datasets, transforms
 import pytorch_fid_wrapper as pfw
 
+import torch
+from torch.autograd import Function
+import numpy as np
+import math
+import torch.linalg as linalg
+from torch.nn.functional import adaptive_avg_pool2d
+from inception import InceptionV3
+
+def calculate_frechet_distance(X):
+    X = X.transpose(0, 1).double()  # [n, b]
+    mu_X = torch.mean(X, dim=1, keepdim=True)  # [n, 1]
+    n, b = X.shape
+    fact = 1.0 if b < 2 else 1.0 / (b - 1)
+
+    # Cov. Matrix
+    E_X = X - mu_X
+    cov_X = torch.matmul(E_X, E_X.t()) * fact  # [n, n]
+    return mu_X, E_X, cov_X
+
+def get_activations(x, model, batch_size=10, dims=2048, device='cuda', num_workers=1):
+    model.eval()
+
+    pred = model(x)[0]
+
+    if pred.size(2) != 1 or pred.size(3) != 1:
+        pred = adaptive_avg_pool2d(pred, output_size=(1, 1))
+
+    return pred.squeeze()
+
+def fid(x, model, batch_size=10, device='cpu', dims=2048):
+    x = get_activations(x, model, batch_size=batch_size, device=device, dims=dims)
+    return calculate_frechet_distance(x, m2, s2)
+
 train_kwargs = {'batch_size': 10000, 'shuffle':True}
 
 transform=transforms.Compose([
@@ -31,16 +64,21 @@ final_cl = torch.stack(final_cl)
 full = final_cl.reshape(-1,3,32,32)
 print(full.shape, final_cl.shape)
 
-pfw.set_config(batch_size=500)
-for i in range(10):
-    print(final_cl[i].shape)
-    c = final_cl[i]
-    c = (c+1)/2
-    real_m, real_s = pfw.get_stats(c)
-    torch.save(real_m, f'real_m_{i}.pt')
-    torch.save(real_s, f'real_s_{i}.pt')
+block_idx = InceptionV3.BLOCK_INDEX_BY_DIM[2048]
+model = InceptionV3([block_idx]).eval()
+
+with torch.no_grad():
+    for i in range(4,10):
+        print(final_cl[i].shape)
+        c = final_cl[i]
+        c = (c+1)/2
+        m, e, c = fid(c, model, batch_size=10)
+        torch.save(m, f'm_{i}.pt')
+        torch.save(e, f'e_{i}.pt')
+        torch.save(c, f'c_{i}.pt')
 
 
-real_m, real_s = pfw.get_stats(full)
-torch.save(real_m, f'real_m_full.pt')
-torch.save(real_s, f'real_s_full.pt')
+    m, e, c = fid(full, model, batch_size=10)
+    torch.save(m, f'm_full.pt')
+    torch.save(e, f'e_full.pt')
+    torch.save(c, f'c_full.pt')
