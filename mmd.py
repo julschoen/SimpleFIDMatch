d4cf0e14772cb072472b6b534e4ce059f6812bc3
julschoen <julianschoen@gmx.de>
2023-02-06 19:45:45 +0100

    Update mmd.py

--- mmd.py
+++ mmd.py
@@ -103,9 +103,9 @@ def _mmd2(K_XX, K_XY, K_YY, const_diagonal=False, biased=False):
     K_XY_sum = K_XY_sums_0.sum()                       # e^T * K_{XY} * e
 
     if biased:
-        mmd2 = ((Kt_XX_sum + sum_diag_X) / (m * m)
-            + (Kt_YY_sum + sum_diag_Y) / (m * m)
-            - 2.0 * K_XY_sum / (m * m))
+        mmd2 = ((Kt_XX_sum + sum_diag_X)
+            + (Kt_YY_sum + sum_diag_Y)
+            - 2.0 * K_XY_sum)
     else:
         mmd2 = (Kt_XX_sum / (m * (m - 1))
             + Kt_YY_sum / (m * (m - 1))
