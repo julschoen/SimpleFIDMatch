27c620b61661c65ef5f5ff408e0df11b27de388d
julschoen <julianschoen@gmx.de>
2023-02-14 11:01:54 +0100

    Update make_comparisons.py

--- make_comparisons.py
+++ make_comparisons.py
@@ -3,20 +3,6 @@ import torch
 from torchvision import datasets, transforms
 import os
 
-# Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    # General Training
-    parser.add_argument('--niter', type=int, default=20000, metavar='N', help='number of epochs to train (default: 14)')
-    parser.add_argument('--lr', type=float, default=1e-3, metavar='LR', help='learning rate (default: 0.001)')
-    parser.add_argument('--num_ims', type=int, default=10)
-    parser.add_argument('--cifar', type=bool, default=True)
-    parser.add_argument('--device', type=str, default='cuda')
-    parser.add_argument('--init_ims', type=bool, default=False)
-    parser.add_argument('--log_dir', type=str, default='./log')
-    parser.add_argument('--mmd', type=bool, default=False)
-    parser.add_argument('--corr', type=bool, default=False)
-    parser.add_argument('--corr_coef', type=float, default=1)
-    args = parser.parse_args()
 
 comp_dir = '../comparison_synth'
 
@@ -58,3 +44,6 @@ def make_random():
 
         if i == 3:
             break
+
+if __name__ == '__main__':
+    make_random()
